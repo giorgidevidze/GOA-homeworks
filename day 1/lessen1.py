b59923217eabb091326a94d5781bfd9287fb8cb8
giorgidevidze <giorgidevidze51@gmail.com>
2024-04-01 00:28:43 +0400

    first commit

---
+++ day 1/lessen1.py
@@ -0,0 +1,33 @@
+name  = "giorgi"
+#name არის ცვლადი
+#=არის ცვლადისთვის მიმნიჭებელი სიმბოლო
+#"giorgi" არის ცვლადისთვის მნიშვნელობა
+
+surname = "devidze"
+
+#print(name)
+#პრინტ უნქციალ გადაეცემა ეკრანზე გამოსატანი ობიექტი
+ 
+name = "giorgi"  # ეს არის str (string) ტიპის ცვლადი 
+age= 15 #ეს არის int (integer) მთელი რიცხვი
+height= 185  #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolena (bool)  ტიპის ცვლადი
+
+knows_programming = True  #True  ან False
+is_ugly = False
+
+isUgly = False # ჯავასკრიპტული camelcase
+  
+
+#print(name + " " + surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+
+
+print(type(age)) 
+print(type(name))
+print(type(surname))
+print(type(height))
+print(type(knows_programming))
+
+print(name + " " + str(age))
---
+++ დავალება/lessen.py
@@ -0,0 +1,15 @@
+name = "giorgi"
+
+surname = "devidze"
+
+age = "15"
+
+height = "160"
+
+
+print(type(age)) 
+print(type(name))
+print(type(surname))
+print(type(height))
+
+print(name + surname + " " +(age + " " + height))
