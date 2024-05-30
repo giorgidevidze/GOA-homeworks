1e6adae98d7cecb07628991f12b27590d2267263
giorgidevidze <giorgidevidze51@gmail.com>
2024-05-30 23:04:30 +0400

    third commit

---
+++ level 4/Classwork/classwork.py
@@ -0,0 +1,47 @@
+sentence = "This is Giorgi's bike"
+
+#text = "Giorgi"
+
+#number = 50
+
+# print(number + 10)
+
+#num1 = "20"
+#num2 = "24"
+
+#print(int(num1) + int(num2))
+
+#num1 = 20
+#num2 = 24
+
+#sum1 = num1 + num2
+
+#print(sum1)
+
+#budget = 200
+
+#print(budget)
+
+#hp = 100
+
+#print(hp)
+#print(hp - 20)
+
+#number = 100 - 11
+
+#print(number)
+
+#num = 10
+#num = 1500
+#num = 2600
+
+#print(num)
+
+# reassign - გადაწერა
+
+# increment - ერთიდ გაზრდა
+
+num = 2
+num = num + 1
+
+print(num)
