5c41456ae12e1e14cfd7460b99aa1c9d4ea706a0
giorgidevidze <giorgidevidze51@gmail.com>
2024-05-31 20:40:33 +0400

    fifth commit

---
+++ level 5/Homework/homework.py
@@ -0,0 +1,41 @@
+# Task 1
+
+dog_name1 = "Rick"
+dog_name2 = "John"
+dog_name2 = "Max"
+
+print("First dog's name is" + " " + dog_name1 + ". " + "Second dog's name is"  +" " + dog_name2 + ". ") 
+
+#In the code, we named the two dogs.
+#This code has also been rewritten.
+
+#Because this code was overwritten, the computer ignored the second line and named the dog Max
+
+#From the best practice we used snake case. Because the words will overlap each other.
+
+
+# Task 2
+# 1
+name = "Giorgi"
+age = "15"
+
+sentence = f"Hello, my name is {name} and I am {age} years old"
+
+print(sentence)
+
+#2
+dog_name = "Luce"
+age = "3"
+
+sentence = f"Hello, my dog's name is {dog_name} and she is {age} years old"
+
+print(sentence)
+
+#3
+name = "Giorgi"
+learn = "Programming"
+sport = "Judoka"
+
+sentence = f"Hello, my name is {name} and I am studying {learn} and I am also a {sport}"
+
+print(sentence)
