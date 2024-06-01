0c0b5bd9ca4278e4ad31f50aa6d9e511570f4b3b
giorgidevidze <giorgidevidze51@gmail.com>
2024-06-01 19:45:27 +0400

    second commit

---
+++ level 6/Classwork/classwork.py
@@ -0,0 +1,6 @@
+# Task 1
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print(num1 + num2)
---
+++ level 6/Homework/homework.py
@@ -0,0 +1,56 @@
+# Task 1
+
+#An integer variable
+num1 = 35
+num2 = 20
+num3 = 10
+
+print(num1 + num2 + num3)
+
+#A string variable
+num1 = ("20")
+num2 = ("2")
+num3 = ("4")
+
+print("20" + "2" + "4")
+
+#input variable
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number:" ))
+num3 = int(input("Enter third number: "))
+
+print( num1 + num2 + num3)
+
+
+
+# Task 2
+#I created an account
+
+#First enter a name
+name = input("Enter your name: ")
+#Second, enter your last name
+surname = input("Enter your surname: ")
+#Enter how old you are in the third
+age = input("Enter your age: ")
+#In the fourth step, enter the password
+password = input("Enter your password: ")
+
+#print(name + surname + age + password)
+
+
+# Task 3
+#I created an account using the f string
+
+#First enter a name
+name = input("Enter your name: ")
+#Second, enter your last name
+surname = input("Enter your surname: ")
+#Enter how old you are in the third
+age = input("Enter your age: ")
+#In the fourth step, enter the password
+password = input("Enter your password: ")
+
+print(f"Name: {name}")
+print(f"Surname: {surname}")
+print(f"Age: {age}")
+print(f"Password: {password}")
