4f5cf91b45376b6675bb36876ed307ffce5268e6
giorgidevidze <giorgidevidze51@gmail.com>
2024-06-18 19:28:05 +0400

    twelfth commit

---
+++ level 10/Classwork/classwork.py
@@ -0,0 +1,17 @@
+# Task 1
+for i in range(50, 100):
+    print(i)
+
+# Task 2
+for i in range(10, 0, -1):
+    print(i)
+
+# # Task 3
+for i in range(100):
+    print("Hello World")
+
+# Task 4
+for i in range(1, 10,):
+    print(i)
+    print("hello word")
+
---
+++ level 10/Homework/homework.py
@@ -0,0 +1,78 @@
+# Task 1
+
+for i in range(0, 102, 2):
+    print(i)
+
+# Task 2
+
+num = 1
+
+while num < 1000:
+    print(num)
+    num = num + 1
+
+
+# Task 3
+
+for i in range(10):
+    print("giorgi")
+
+
+for i in range(35):
+    print(i)
+
+
+for i in range(25, 100, 3):
+    print(i)
+
+
+for i in range(1, 5):
+    print("giorgi")
+    print("devidze")
+
+
+for i in range(-100, 0, 1):
+    print(i)
+
+
+
+num = 1
+
+while num < 100:
+    print(num)
+    num = num + 1
+
+
+num = 1
+num1 = 20
+
+while num < 20:
+    print(num)
+    num = num + 1
+while num1 < 40:
+    print(num1)
+    num1 = num1 + 1
+
+
+num = 15
+
+while num < 50:
+    print("Hello Word")
+    num = num + 1
+    
+
+num = 1
+num2 = 1
+
+while num < 2:
+    print("Hello")
+    num = num + 1
+while num2 < 2:
+    print("my name giorgi")
+    num2 = num2 + 1
+
+num = 1
+
+while num < 10:
+    print("Goal Oriented Academia")
+    num = num + 1
