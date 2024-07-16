1e2926e0123566f93f0d234e8cf601a4567d028e
giorgidevidze <giorgidevidze51@gmail.com>
2024-07-16 21:31:29 +0400

    twentieth commit

---
+++ level 18/Classwork/classwork.py
@@ -0,0 +1,60 @@
+# task 1
+
+def greeting(name):
+    print(f"hello {name}!")
+
+greeting("giorgi")
+
+
+# task 2
+
+def number(num1, num2, num3):
+    print(num1 + num2 + num3)
+
+number(10, 10, 10)
+
+# task 3
+
+def number(start, end):
+    for i in range(start, end):
+        print(i)
+
+number(1, 15)
+
+
+# task 4
+
+def greeting(name, surname, academy):
+    print(f"My name is {name}")
+    print(f"my surname is {surname}")
+    print(f"I study in {academy}")
+
+greeting("giorgi", "devidze", "Goa")
+
+
+# task 5
+
+def number(num1, num2, num3):
+    print(num1)
+
+x = (10 * 10 * 10 )
+
+
+# task 6
+
+def multiplier(num1, num2, num3):
+    return num1 * num2 * num3
+
+result = multiplier(10, 10, 10)
+
+print(result)
+
+
+# task 7 
+
+def list_print(list1):
+    for i in list1:
+        print(i)
+
+list_print([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
---
+++ level 18/Homework/homework.py
@@ -0,0 +1,60 @@
+# task 1
+
+def number(num1, num2):
+    print(num1 + num2)
+
+number(13, 17)
+
+
+# task 2
+
+def number(num1, num2, num3):
+    return num1 * num2 * num3
+
+x = number(20, 20, 10)
+
+print(x)
+
+
+# task 3
+
+def greeting(name, surname):
+    print(f"hello {name} {surname}")
+
+greeting("giorgi", "devidze")
+
+
+# task 4
+
+def sum (num1, num2):
+    return num1 + num2
+
+num3 = sum(10, 10)
+print(num3)
+
+
+# task 5
+
+def sum (num1, num2):
+    return num1 * num2
+
+num3 = sum(10, 10)
+print(num3)
+
+
+# task 6
+
+def foods(list1):
+    for i in list1:
+        print(i)
+
+foods(["xinkali", "lobiani", "mwvadi", "xachapuri"])
+
+
+# task 7
+
+def numbers(list1):
+    for i in list1:
+        print(i)
+
+numbers([3, 4, 5, 6, 9])
