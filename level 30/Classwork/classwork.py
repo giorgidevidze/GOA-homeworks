e5fae7a27a89a881712d06f8104909a8f74c3d1b
giorgidevidze <giorgidevidze51@gmail.com>
2024-08-25 15:11:36 +0400

    27 commit

---
+++ level 30/Classwork/classwork.py
@@ -0,0 +1,52 @@
+# Task 1
+
+car = {
+    "color" : "black",
+    "brand" : "mersedes",
+    "year" : "2010"
+}
+
+print(car)
+
+car = {
+    "color" : "black",
+    "brand" : "mersedes",
+    "year" : "2010"
+}
+
+print(car.get("color"))
+
+
+# Task 2
+
+car = {
+    "color" : "black",
+    "brand" : "mersedes",
+    "year" : "2010"
+    }
+
+for i in car:
+    print(i)
+
+
+
+# Task 3
+
+car = {
+    "color" : "black",
+    "brand" : "mersedes",
+    "year" : "2010"
+}
+
+print(list(car.values()))
+
+
+# Task 4
+
+car = {
+    "color" : "black",
+    "brand" : "mersedes",
+    "year" : "2010"
+}
+
+print(car.values())
