2703eeb8bf17f43277ad8faef94a661376cfa946
giorgidevidze <giorgidevidze51@gmail.com>
2024-11-21 17:04:08 +0400

    38 commit

---
+++ level 53/Homework/script.js
@@ -0,0 +1,13 @@
+//  Task 2
+
+let num1 = 10;
+let num2 = 10;
+let sum = num1 + num2;
+console.log("Sum :", sum);
+
+
+//  Task 3
+
+let text1 = "sea";
+let text2 = "food";
+let result = text1.concat(text2);
